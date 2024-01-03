9f253d478527a8922b086decba3be4842df47151
Ranvir Chhina <ranvirchhina25@gmail.com>
2024-01-03 09:49:02 -0800

    Time: 41 ms (94.11%) | Memory: 18.3 MB (19.48%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
@@ -1,31 +1,17 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
-    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        dummy = ListNode()
-        prev, curr = dummy, head
-        while curr:
-            temp = curr
-            headNextk, curr, n = self.reverseNextK(curr, k)
-            if n < k:
-                headNextk, curr, _ = self.reverseNextK(headNextk, k)
-            prev.next = headNextk
-            prev = temp
-        return dummy.next
-
-    def reverseNextK(self, curr, k):
-        """Return the (head of reversed next k, next pointer, length of reversed segment)
-        """
-        prev = None
-        n = 0
-        for i in range(1, k + 1):
-            if not curr:
-                break
-            n = i
-            temp, prev, curr = prev, curr, curr.next
-            prev.next = temp
-        return prev, curr, n
-
+    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
+        count = 0
+        curr = head
+        while curr and count < k:
+            curr = curr.next
+            count += 1
+        if count < k:
+            return head
+        prev, curr = None, head
+        for _ in range(k):
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        head.next = self.reverseKGroup(curr, k)
+        return prev
