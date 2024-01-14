d8902bfd1ddb6b1948b19eaac09d0e50cf42019a
Ranvir Chhina <ranvirchhina25@gmail.com>
2024-01-14 11:51:58 -0800

    Time: 62 ms (74.17%) | Memory: 25.4 MB (81.77%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -5,26 +5,21 @@
 #         self.next = next
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
-        if not head or not head.next:
-            return
-
-        half = end = head
-        # find halfway point
-        while end.next and end.next.next:
-            half = half.next
-            end = end.next.next
-
-        # split lists and reverse part2
-        prev, curr, half.next = None, half.next, None
-        while curr:
-            temp, prev, curr = prev, curr, curr.next
-            prev.next = temp
-        # interleave lists
-        while prev:
-            temp1, temp2 = head.next, prev.next
-            head.next = prev
-            prev.next = temp1
-            head = temp1
-            prev = temp2
-
-        
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        slow, fast = head, head
+        while fast.next and fast.next.next:
+            slow, fast = slow.next, fast.next.next
+        half, slow, fast = slow, None, slow.next
+        half.next = None
+        while fast:
+            temp, slow, fast = slow, fast, fast.next
+            slow.next = temp
+        a, b = head, slow
+        while a and b:
+            temp1, temp2 = a.next, b.next
+            a.next = b
+            b.next = temp1
+            a, b = temp1, temp2
+        return head
