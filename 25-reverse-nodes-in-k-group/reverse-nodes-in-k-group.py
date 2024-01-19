78bdbb9ee4adf5d01f08469e2801df056db62708
Ranvir Chhina <ranvirchhina25@gmail.com>
2024-01-19 10:11:44 -0800

    Time: 48 ms (60.12%) | Memory: 17.5 MB (89.30%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
@@ -1,17 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
-        count = 0
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        # first pass: calculate length to determine number of k groups
         curr = head
-        while curr and count < k:
+        n = 0
+        while curr:
+            n += 1
             curr = curr.next
-            count += 1
-        if count < k:
-            return head
-        prev, curr = None, head
-        # reverse first k group
-        for _ in range(k):
-            temp, prev, curr = prev, curr, curr.next
-            prev.next = temp
-        # recursively reverse rest of valid k group
-        head.next = self.reverseKGroup(curr, k)
-        return prev
+
+        # second pass: reverse one k group at a time and join it to previous tail
+        dummy = ListNode()
+        curr, tail = head, dummy
+        for _ in range(n // k):
+            new_tail, prev = curr, None
+            for _ in range(k):
+                temp, prev, curr = prev, curr, curr.next
+                prev.next = temp
+            tail.next = prev
+            tail = new_tail
+        
+        # if left-out nodes, join them to the tail
+        if curr:
+            tail.next = curr
+        
+        return dummy.next
+            
