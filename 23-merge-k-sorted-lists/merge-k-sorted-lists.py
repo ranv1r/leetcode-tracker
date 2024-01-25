969c42769dbd559a817a2cd8e25504ab391ce0f5
Ranvir Chhina <ranvirchhina25@gmail.com>
2024-01-25 10:26:11 -0800

    Time: 68 ms (96.89%) | Memory: 19.4 MB (97.93%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -5,28 +5,35 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if not lists:
+        if len(lists) == 0:
             return None
-        def merge(l1, l2):
-            dummy = ListNode()
-            curr = dummy
-            while l1 and l2:
-                if l1.val > l2.val:
-                    l1, l2 = l2, l1
-                curr.next = l1
-                l1 = l1.next
-                curr = curr.next
-            if l1:
-                curr.next = l1
-            if l2:
-                curr.next = l2
-            return dummy.next
-        while len(lists) > 1:
-            new_lists = [] 
-            i = 0
-            while i < len(lists):
-                new_list = merge(lists[i], lists[i + 1]) if i + 1 < len(lists) else lists[i]
-                new_lists.append(new_list)
-                i += 2
-            lists = new_lists
-        return lists.pop()
+        return self.mergeLists(lists)
+
+    def mergeLists(self, lists):
+        if len(lists) == 1:
+            return lists[0]
+        if len(lists) == 2:
+            return self.mergeTwoLists(lists[0], lists[1])
+        half = len(lists) // 2
+        leftHalf, rightHalf = lists[:half], lists[half:]
+
+        return self.mergeTwoLists(
+            self.mergeLists(leftHalf), 
+            self.mergeLists(rightHalf)
+        )
+        
+
+    def mergeTwoLists(self, node1, node2):
+        dummy = ListNode()
+        res = dummy
+        while node1 and node2:
+            node1, node2 = (node1, node2) if node1.val <= node2.val else (node2, node1)
+            res.next = node1
+            node1 = node1.next
+            res = res.next
+        if node1:
+            res.next = node1
+        if node2:
+            res.next = node2
+        return dummy.next
+            
