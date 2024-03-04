ab1e4255c2dff02489520be75fdc8cf4ffa1a07f
Ranvir Chhina <ranvirchhina25@gmail.com>
2024-03-04 07:17:57 -0800

    Time: 567 ms (64.05%) | Memory: 77.7 MB (79.50%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,51 +1,76 @@
-class Node:
-    def __init__(self, key = 0, next = None, prev = None):
-        self.key = key # (key, val) tuple
-        self.next = next
-        self.prev = prev
+class ListNode:
+    def __init__(self, key = 0, value = 0, next_node = None, prev_node = None):
+        self.key = key
+        self.value = value
+        self.next = next_node
+        self.prev = prev_node
 
 class LRUCache:
+    """
+    Stores key-value pairs. An integer key is mapped to a Double-linked list node using a dictionary.
+    """
 
     def __init__(self, capacity: int):
-        self.mru = Node() # Most Recently Used
-        self.lru = Node() # Least Recently Used
-        self.lru.next = self.mru
-        self.mru.prev = self.lru
-        self.cache = {}
-        self.cap = capacity
-
-    def insert(self, node):
-        left, right = self.mru.prev, self.mru
-        left.next = node
-        node.prev = left
-        right.prev = node
-        node.next = right
+        self.cache_dict = {}
+        self.capacity = capacity
+        self.left = ListNode() # left is least recently used
+        self.right = ListNode() # right is most recently used
+        
+        # Setting up empty linked list with two dummy nodes on edges
+        self.left.next = self.right
+        self.right.prev = self.left
     
-    def delete(self, node):
-        left, right =  node.prev, node.next
-        left.next = right
-        right.prev = left
+    def remove(self, node):
+        l, r = node.prev, node.next
+        l.next = r
+        r.prev = l
+    
+    def push(self, node):
+        l, r = self.right.prev, self.right
+        l.next = node
+        node.prev = l
+        node.next = r
+        r.prev = node
+    
+    def evict(self):
+        key = self.left.next.key
+        del self.cache_dict[key]
+        self.remove(self.left.next)
 
     def get(self, key: int) -> int:
-        if key not in self.cache:
-            return -1
-        node, value = self.cache[key]
-        self.delete(node)
-        self.insert(node)
-        return value
+        # look up cache_dict for key
+        result = self.cache_dict.get(key, -1)
+        # if not exists return -1
+        if result == -1:
+            return result
+        # remove from linked list
+        self.remove(result)
+        # add to right edge of linked list
+        self.push(result)
+        return result.value
         
 
     def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            node, _ = self.cache[key]
-            self.delete(node)
+        # case 1: key already exists
+        if key in self.cache_dict:
+            result = self.cache_dict[key]
+            # remove from list
+            self.remove(result)
+            # update value
+            result.value = value
+            # add to list right edge
+            self.push(result)
+        # case 2: key does not exist
         else:
-            node = Node(key)
-        self.insert(node)
-        self.cache[key] = (node, value)
-        if len(self.cache) > self.cap:
-            del self.cache[self.lru.next.key]
-            self.delete(self.lru.next)
+            # add to the right edge of the list
+            new_node = ListNode(key, value)
+            self.push(new_node)
+            self.cache_dict[key] = new_node
+            # if new size exceeds capacity
+            if len(self.cache_dict) > self.capacity:
+                # evict element from left edge
+                self.evict()
+        
 
 
 # Your LRUCache object will be instantiated and called as such:
