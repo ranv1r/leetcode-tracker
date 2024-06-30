719bee82a3994253d8d5ce9b5242f8dd24f6c9c8
Ranvir Chhina <ranvirchhina25@gmail.com>
2024-06-30 07:15:46 -0700

    Time: 97 ms (19.71%) | Memory: 20.6 MB (97.11%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,15 +1,24 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # Calculate the counts
         counts = defaultdict(int)
         for x in nums:
             counts[x] += 1
-        buckets = defaultdict(list)
+
+        # Calculate the max count
+        count_max = max(counts.values())
+
+        # Rearrange into bucktes
+        buckets = [[] for _ in range(count_max + 1)]
         for x, count in counts.items():
             buckets[count].append(x)
-        res = []
-        i = len(nums)
-        while len(res) != k:
-            if buckets[i]:
-                res.extend(buckets[i])
-            i -= 1
-        return res
+
+        # Iterate buckets in reverse order
+        j = count_max # iteration index of buckets
+        ret = []
+        for _ in range(k):
+            while len(buckets[j]) == 0:
+                j -= 1
+            ret.append(buckets[j].pop())
+        return ret
+            
